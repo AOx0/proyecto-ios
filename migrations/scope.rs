911502ad1a5f296e7f5d2f322e37fc45fe76f385
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-05-10 18:28:52 -0600

    Changes

--- migrations/scope.rs
+++ migrations/scope.rs
@@ -7,7 +7,6 @@ DEFINE SCOPE account SESSION 24h
         SET
             email = $email,
             pass = crypto::argon2::generate($pass),
-            collections = [],
             first_name="",
             last_name=""
     )
@@ -25,8 +24,6 @@ CREATE user:daniel
         pass = crypto::argon2::generate("1234"),
         gravatar = "danielosorniolopez@gmail.com",
         gravatar_md5 = crypto::md5("danielosorniolopez@gmail.com"),
-        collections = [collection:col1],
-        suscriptions = [],
         first_name="Daniel",
         last_name="Osornio"
 ;
@@ -43,21 +40,17 @@ CREATE user:david
     SET
         email = "david@gmail.com",
         pass = crypto::argon2::generate("1234"),
-        collections = [],
-        suscriptions = [collection:col1],
-        ps = true,
-        first_name="David",
-        last_name="Osornio"
+        public_sus = true,
+        first_name="Hector",
+        last_name="Espinoza"
 ;
 
 CREATE user:agus
     SET
         email = "agus@gmail.com",
         pass = crypto::argon2::generate("1234"),
-        collections = [],
-        suscriptions = [collection:col1],
         first_name="Agus",
-        last_name="Osornio"
+        last_name="Lopez"
 ;
 
 COMMIT;
--- migrations/user.rs
+++ migrations/user.rs
@@ -1,16 +1,14 @@
 /// user:
+///     id: record(user)
 ///     first_name: string
 ///     last_name: string
 ///     email: string
 ///     pass: string
 ///     gravatar: string
 ///     gravatar_md5: string
-///     collections: [record(collection)]
-///     saved: [record(collection)]
 ///
 BEGIN;
 
-// Borrar la tabla si ya existe
 DELETE user;
 
 // Solo los usuarios pueden modificar su información, pero todos pueden ver.
@@ -18,75 +16,48 @@ DELETE user;
 // Por eso el select tiene FULL permissions y el resto de acciones solo se 
 // pueden realizer si el id de la persona que lo quiere realizar es igual 
 // al del usuario que se quiere modificar
-DEFINE TABLE user
+DEFINE TABLE user SCHEMAFULL
     PERMISSIONS
-        FOR select
-            FULL
-        FOR create, update, delete
-		        WHERE id = $auth.id
-;
-
-// El email solo es accessible para el dueño de la cuenta, privado para el
-// resto de personas
-DEFINE FIELD email ON TABLE user TYPE string
-    PERMISSIONS
-        FOR select, create, update, delete
-		    WHERE id = $auth.id
-    ASSERT 
-        // El email no puede ser nulo
-        $value != NONE
-        // El email debe ser un string correctamente formateado
-        AND is::email($value)
-;
-
-// La contrase√±a puede ser actualizada por el usuario pero no vista
-DEFINE FIELD pass ON TABLE user
-    PERMISSIONS
-        FOR select
-            NONE
-        FOR create, update, delete
-		        WHERE id = $auth.id
+        FOR select FULL
+        FOR create, update, delete WHERE id = $auth.id
 ;
 
 // El id no es modificable, el usuario no puede verlo ni actualizarlo
-DEFINE FIELD id ON TABLE user 
-    PERMISSIONS
-        FOR select FULL
-        FOR create, update, delete NONE
+DEFINE FIELD id ON TABLE user PERMISSIONS 
+    FOR select, create, update, delete NONE
 ;
 
+// Define first_name and last_name
+DEFINE FIELD first_name ON TABLE user TYPE string;
+DEFINE FIELD last_name ON TABLE user TYPE string;
 
-// Las otras personas solo pueden ver tus suscripciones si lo tienes
-// marcado como publico
-DEFINE FIELD suscriptions ON TABLE user
+// 1. Solo es accessible para el dueño de la cuenta
+// 2. El email no puede ser nulo y debe ser valido
+DEFINE FIELD email ON TABLE user TYPE string
     PERMISSIONS
-        FOR select
-            WHERE id = $auth.id OR ps = true
-        FOR create, update, delete
-            WHERE id = $auth.id
-;
-
-// Por defecto las suscripciones son privadas
-DEFINE FIELD ps ON TABLE user    
-    VALUE $value OR false
-;
-
-// Todos pueden ver tu foto de perfil
-DEFINE FIELD gravatar_md5 ON TABLE user    
-    VALUE $value OR ""
-    PERMISSIONS
-        FOR select FULL
-        FOR create, update, delete
-            WHERE id = $auth.id
+        FOR select WHERE id = $auth.id
+    ASSERT 
+        $value != NONE AND is::email($value)
 ;
 
 // Solo tu puedes ver el email de gravatar
-DEFINE FIELD gravatar ON TABLE user    
+DEFINE FIELD gravatar ON TABLE user
     VALUE $value OR ""
     PERMISSIONS
-        FOR select, create, update, delete
-            WHERE id = $auth.id
+        FOR select WHERE id = $auth.id
 ;
 
+// Todos pueden ver tu foto de perfil
+DEFINE FIELD gravatar_md5 ON TABLE user
+    VALUE $value OR ""
+    PERMISSIONS
+        FOR select FULL
+;
+
+// La contraseña puede ser actualizada por el usuario pero no vista
+DEFINE FIELD pass ON TABLE user PERMISSIONS FOR select NONE;
+
+// Por defecto las suscripciones son privadas
+DEFINE FIELD public_sus ON TABLE user VALUE $value OR false;
 
 COMMIT;
